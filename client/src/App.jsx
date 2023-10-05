777031ae479163d9e1dafa408b110ab971dba443
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2023-10-05 12:07:56 +0530

    commit profile page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
+// App.jsx
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/Signin';
-import Header from './components/header';
-import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
+import SignUp from './pages/SignUp.jsx';
+import SignIn from './pages/SignIn.jsx';
+import Header from './components/Header.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 export default function App() {
   return (
@@ -17,8 +18,8 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} /> 
         <Route path="/about" element={<About />} />
-        <Route element={PrivateRoute}>
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
