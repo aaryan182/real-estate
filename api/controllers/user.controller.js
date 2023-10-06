9099ec75f8a2503eca14a9caa838dd0b09a6bed7
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2023-10-06 19:04:13 +0530

    complete show user listing  functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,11 +1,11 @@
-import bcryptjs from "bcryptjs";
-import User from "../models/user.model.js";
-import { errorHandler } from "../utils/error.js";
-import Listing from "../models/listing.model.js";
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 export const test = (req, res) => {
   res.json({
-    message: "Api route is working ",
+    message: 'Api route is working!',
   });
 };
 
@@ -38,30 +38,27 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only delete your own account!'));
   try {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie('access_token');
-    res.status(200).json('User deleted successfully!');
+    res.status(200).json('User has been deleted!');
   } catch (error) {
     next(error);
   }
 };
 
-
 export const getUserListings = async (req, res, next) => {
-
-  if (req.user.id === req.params.id){
+  if (req.user.id === req.params.id) {
     try {
       const listings = await Listing.find({ userRef: req.params.id });
       res.status(200).json(listings);
     } catch (error) {
       next(error);
     }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
   }
-  else return next(errorHandler(401, 'You can only get your own listings!'));
-  
-}
+};
